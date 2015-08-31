aefb2cb39440200fd496a363566561ea27c9b315
NKUCodingCat <chenjingtao930@gmail.com>
2015-08-31 11:43:39 +0800

    C#

--- C#/2015.cs
+++ C#/2015.cs
@@ -12,7 +12,7 @@ namespace 2015
         static Func<T1, TResult> Make<T1, TResult>(SelfApplicable<T1, TResult> self){ return (x) => self(self, x);}
         static void Main(string[] args)
         {
-            System.Console.WriteLine((Make<long, String>((__, ___) => (___ > 0) ? ((char)(___ % 256) + __(__, ___ >> (0x10 >> 1))) : ""))((((3 << 5) + 1) << ((7 << 3))) + (((((1 << 4) - 1) << 2) - 1) << ((3 << 4) + 1)) + (((3 << 4) + 1) << ((5 << 3) + 1)) - (((((3 << 2) - 1) << 2) + 1) << ((1 << 5) + (1 << 1))) - (((1 << 4) - 1) << ((7 << 2) + 1)) + (((7 << 2) + 1) << ((9 << 1))) + (7 << ((3 << 2))) - ((((3 << 2) - 1)) << 4) - (1 << 1)));
+            System.Console.WriteLine((Make<long, String>((__, ___) => (___ > 0) ? ((char)(___ % 256) + __(__, ___ >> (0x10 >> 1))) : ""))(0x6176614a20746f4e));
         }
     }
 }
