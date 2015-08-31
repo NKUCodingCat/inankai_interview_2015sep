c5e16dcce82fb7b27e0e2b3bfe42902ecf391e73
NKUCodingCat <chenjingtao930@gmail.com>
2015-08-31 11:19:10 +0800

    CSharp

---
+++ C#/2015.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 2015
+{
+    class Program
+    {
+        delegate TResult SelfApplicable<T1, TResult>(SelfApplicable<T1, TResult> self, T1 arg1);
+        static Func<T1, TResult> Make<T1, TResult>(SelfApplicable<T1, TResult> self){ return (x) => self(self, x);}
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine((Make<long, String>((__, ___) => (___ > 0) ? ((char)(___ % 256) + __(__, ___ >> (0x10 >> 1))) : ""))(7022907639930842958));
+        }
+    }
+}
