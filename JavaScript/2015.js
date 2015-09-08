fcb3ee2bfc7525f7419503768d8d2ea7ffdeccf8
NKUCodingCat <chenjingtao930@gmail.com>
2015-09-08 20:21:25 +0800

    JS

---
+++ JavaScript/2015.js
@@ -0,0 +1,25 @@
+/*
+ *Chrome 39.0.2171.95 m
+ *Author: secreter<1575260062@qq.com>
+*/
+var Ma1 = {
+    love: "\x69\x6e\x61\x6e\x6b\x61\x69",
+    you: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    j: function(z2) {
+        return z2["\x74\x6f\x53\x74\x72\x69\x6e\x67"]()["\x63\x68\x61\x72\x43\x6f\x64\x65\x41\x74"](0)
+    },
+    s: function(sVudAOy$3) {
+        return window["\x53\x74\x72\x69\x6e\x67"]["\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65"](sVudAOy$3)
+    }
+}
+var GZ4 = (function() {
+    need = [];
+    for (var F5 = 0; F5 < 30; F5++) {
+        need["\x70\x75\x73\x68"](Ma1["\x73"](Ma1["\x6a"](Ma1["\x79\x6f\x75"][F5 % 10]) + 16 + window["\x70\x61\x72\x73\x65\x49\x6e\x74"](F5 / 10) * 10));
+    }
+    return need;
+})();
+Ma1["\x6e\x61\x6e\x6b\x61\x69"] = function() {
+    window["\x61\x6c\x65\x72\x74"](GZ4[23] + GZ4[5] + GZ4[12] + GZ4[3] + GZ4[15] + GZ4[13] + GZ4[5] + " \x74\x6f " + Ma1["\x6c\x6f\x76\x65"] + "\x2c" + Ma1["\x73"](114) + Ma1["\x73"](101) + Ma1["\x73"](115) + Ma1["\x73"](117) + Ma1["\x73"](109) + Ma1["\x73"](101) + " \x74\x6f " + Ma1["\x6c\x6f\x76\x65"] + GZ4[0] + Ma1["\x6c\x6f\x76\x65"]["\x73\x75\x62\x73\x74\x72\x69\x6e\x67"](1) + "\x2e\x65\x64\x75\x2e\x63\x6e");
+}
+Ma1["\x6e\x61\x6e\x6b\x61\x69"]();
