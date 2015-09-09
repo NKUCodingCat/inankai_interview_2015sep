3be7c87b7563a30d2b20b7bd3f98ff2d0ea64a5d
NKUCodingCat <chenjingtao930@gmail.com>
2015-09-09 16:50:00 +0800

    C

---
+++ C/2015.c
@@ -0,0 +1,25 @@
+/*
+	*Microsoft Visual Studio Ultimate 2012 
+	*Version 11.0.61030.00 Update 4
+	*Author: hjy-sky<871683984@qq.com>
+*/
+#include <stdio.h>
+int __(int _){ return(++_); }
+int ___(int*_,int*__,int*___){return(*___=(*_)*(*__));}
+typedef int(*Set1)(int in);      
+typedef int(*Set2) (int*,int*,int*);   
+void _(Set2 ____,int _w_, int*_vv_)
+{
+	int k, j[7]={5,32,13,0,35,13,5};
+	Set1 p=__;
+	int temp =p(_w_);
+	____(&temp,&_w_, _vv_);
+	for(k=0;k<7;k++)
+		printf("%c",*_vv_-j[k]);
+}
+main()
+{
+	int _____;
+	_(___,10,&_____);
+	return 0;
+}
